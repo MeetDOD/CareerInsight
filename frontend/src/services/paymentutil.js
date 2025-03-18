695f817ac77e3d1cf5a7d33e6434c8754df68839
Sumeshyadav10 <ysumesh084@gmail.com>
2025-03-19 00:53:43 +0530

    bug fix

--- frontend/src/services/paymentutil.js
+++ frontend/src/services/paymentutil.js
@@ -1,9 +1,13 @@
 import { load } from "@cashfreepayments/cashfree-js";
 
+let cashfree;
 
-export const cashfree = await load({
-	mode: "sandbox" //or production
-});
-//This load function returns a Promise that resolves with a newly created Cashfree object once Cashfree.js has loaded. If you call load in a server environment it will resolve to null.
+const initializeCashfree = async () => {
+    cashfree = await load({
+        mode: "sandbox" // or "production"
+    });
+};
 
+initializeCashfree();
 
+export { cashfree };
